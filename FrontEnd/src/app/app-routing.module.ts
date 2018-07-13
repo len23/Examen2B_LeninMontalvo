8ef81037af32a0e8a33e1bf4bffb481e057020b0
Unknown <lenin.montalvo.23@hotmail.com>
2018-07-13 03:57:20 -0500

    Avance

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TiendasPadreComponent} from './tiendas-padre/tiendas-padre.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: TiendasPadreComponent }
+
+];
 
 @NgModule({
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
+
 })
 export class AppRoutingModule {} 
